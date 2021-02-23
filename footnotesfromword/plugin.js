c583881351b0835904bcc89fff5ad4ac45e5e4dc
Andy Kirk <andy@andykirk.net>
2021-02-23 17:37:10 +0000

    Remove jQuery dependancy.

--- footnotesfromword/plugin.js
+++ footnotesfromword/plugin.js
@@ -1,77 +1,91 @@
 /**
  * Plugin to convert pasted Word footnotes in CKEditor Footnotes format.
  *
- * 
+ *
  */
 
 // Register the plugin within the editor.
 CKEDITOR.plugins.add( 'footnotesfromword', {
     requires: 'footnotes,pastefromword',
-    
+
     init: function(editor) {
         editor.on( 'paste', function( evt ) {
-            //console.log(evt.editor);
+
             var data = evt.data;
-            
+
             // Check if there are any Word footnotes:
-            if (!(/<a href="#_ftn/).test(data.dataValue)) {
+            if (!(/<a[^>]*href="#_ftn/).test(data.dataValue)) {
                 return;
             }
+
             // Cancel the paste event:
             evt.cancel();
             editor.fire('lockSnapshot');
-            var $existing_data = jQuery('<div/>').html(editor.getData());
-            var contents = jQuery('<div/>').html(data.dataValue); //jQuery('<div>' + data.dataValue + '</div>');
+
+            var existing_data = CKEDITOR.dom.element.createFromHtml('<div>');
+            existing_data.setHtml(editor.getData());
+
+            var contents = CKEDITOR.dom.element.createFromHtml('<div>');
+            contents.setHtml(data.dataValue);
+
             var footnotes = '';
-            contents.find('a[name]').each(function(){
-                $this = jQuery(this);
-                
+            contents.find('a[name]').toArray().forEach(function(item){
+
                 // Remove unnecessary named anchors e.g. OLE_LINK1:
-                if ($this.attr('name').match(/^OLE_LINK/)) {
-                    $this.replaceWith($this.html());
+                if (item.getAttribute('name').match(/^OLE_LINK/)) {
+                    console.log(item);
+                    item.remove();
                 }
-                
+
                 // Start to build the footnote:
-                if ($this.attr('name').match(/^_ftnref/)) {
-                    // Get the incoming footnote text ready of existance check:
-                    var $footnote_p = jQuery(contents.find('a[href="#' + $this.attr('name') + '"]').parent());
-                    var footnote_text = $footnote_p.html().replace(/^<a[^>]+>[^<]+<\/a>/, '');
-                    
+                if (item.getAttribute('name').match(/^_ftnref/)) {
+                    // Get the incoming footnote text ready for existance check:
+                    var footnote_p = contents.findOne('a[href="#' + item.getAttribute('name') + '"]').getParent();
+                    var footnote_text = footnote_p.getHtml().replace(/^<a[^>]+>[^<]+<\/a>/, '');
+
                     // Add the text of any sibling p tags up to the start of the next ref:
-                    $footnote_p.nextUntil('p > a[name]').each(function(){
-                        footnote_text += jQuery(this).html();
-                        jQuery(this).remove();
-                    });
-                    $footnote_p.remove();
-                    
-                    footnote_text = jQuery.trim(footnote_text.replace(/&nbsp;/g, ' ').replace(/(\n|\r)/g, ' ').replace(/\s{2,}/g, ' '));
-                
+                    var t = footnote_p;
+                    while (t.hasNext()) {
+                        t = t.getNext();
+
+                        if (t.findOne('> a[name]')) {
+                            break;
+                        } else {
+                            footnote_text += t.getHtml();
+                        }
+                    }
+                    footnote_p.remove();
+
+                    footnote_text = CKEDITOR.tools.trim(footnote_text.replace(/&nbsp;/g, ' ').replace(/(\n|\r)/g, ' ').replace(/\s{2,}/g, ' '));
+
+                    // Remove any Word [n] anchors from the footnotes:
+                    footnote_text = footnote_text.replace(/<a[^>]*href="#_ftnref[^>]+>.*?<\/a>/g, '');
+
                     // Check for pre-existence footnote:
-                    var footnote_id = false
-                    $existing_data.find('cite').each(function(){
-                        var $this = jQuery(this);
-                        if ($this.text() == footnote_text) {
-                            footnote_id = $this.parent().attr('data-footnote-id');
+                    var footnote_id = false;
+                    existing_data.find('cite').toArray().forEach(function(item){
+                        if (item.getText() == footnote_text) {
+                            footnote_id = item.getParent().attr('data-footnote-id');
                             return false;
                         }
                     });
-                    
+
                     if (!footnote_id) {
                         footnote_id = editor.plugins.footnotes.generateFootnoteId();
                         footnotes += '<li data-footnote-id="' + footnote_id + '"><cite>' + footnote_text + '</cite></li>';
                     }
-                    
-                    var footnote_marker = '<sup data-footnote-id="' + footnote_id + '">X</sup>';
-                    $this.replaceWith(footnote_marker);
+
+                    var footnote_marker = CKEDITOR.dom.element.createFromHtml('<sup data-footnote-id="' + footnote_id + '">X</sup>');
+                    footnote_marker.replace(item);
                 }
             });
-            var new_data = contents.html();    
+            var new_data = contents.getHtml();
             editor.insertHtml(new_data);
             data = editor.getData();
-            
+
             // Tidy up some things:
             data = data.replace(/<p>&nbsp;<\/p>\n*<hr \/>/, '');
-            
+
             // Add footnotes section if it doesn't exist:
             if ((/<section.*class="footnotes"/).test(data)) {
                 data = data.replace(/(<section.*class="footnotes"[^]*)<\/ol>[^]*?<\/section>/, '$1' + footnotes + '</ol></section>');
